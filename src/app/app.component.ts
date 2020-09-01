d30258bcc04a646b0eaf79a9dd06a64f752c4e41
ivp.srath mumbai <srath@ivp.in>
2020-09-01 22:36:41 +0530

    tile added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'secmaster';
+  bondselect(){
+    document.getElementById("button1").style.backgroundColor="white";
+    document.getElementById("button2").style.backgroundColor="gray";
+  }
+  equityselect(){
+    document.getElementById("button2").style.backgroundColor="white";
+    document.getElementById("button1").style.backgroundColor="gray";
+  }
 }
